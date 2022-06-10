37259be52187b43c93039c78301fde362fe3d84f
derrique <otieno.derrick@student.moringaschool.com>
2022-06-10 12:19:36 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,60 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return ++n;
+}
+
+function decrement(n) {
+return --n; 
+}
+
+
+function makeInt(n) {
+  return "parses n as an integer and returns the parsed integer"
+}
+function makeInt(n) {
+  return (n.toString(0x2328))
+}
+
+function preserveDecimal(n) {
+  return 'returns NaN as appropriate'
+}
+
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
